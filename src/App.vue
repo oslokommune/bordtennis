47662458443ee994d08232c3a1f2e7a2cd526e30
Julius Pedersen <deifyed@tutanota.com>
2022-11-05 15:17:20 +0100

    ✅ Add basic scoreboard number design and functionality

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,44 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import ScoreboardNumber from './components/ScoreboardNumber.vue'
+
+export default {
+	name: 'App',
+	components: {
+		ScoreboardNumber,
+	},
+	data() {
+		return {
+			message: 'Hello Vue!'
+		}
+	}
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<div class="player-side">
+		<ScoreboardNumber :number="0" />
+		<ScoreboardNumber :number="0" />
+	</div>
+
+	<div class="player-side">
+		<ScoreboardNumber :number="0" />
+		<ScoreboardNumber :number="0" />
+	</div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+.ScoreboardNumber {
+	height: 100%;
+
+	width: 48%;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.player-side {
+	height: 100%;
+	width: 45%;
+
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
 }
 </style>
---
+++ src/components/ScoreboardNumber.vue
@@ -0,0 +1,91 @@
+<template>
+	<div class="ScoreboardNumber">
+		<div class="top" :class="{active: top}">&nbsp;</div>
+		<div class="upper-left" :class="{active: upperLeft}">&nbsp;</div>
+		<div class="upper-right" :class="{active: upperRight}">&nbsp;</div>
+		<div class="middle" :class="{active: middle}">&nbsp;</div>
+		<div class="lower-left" :class="{active: lowerLeft}">&nbsp;</div>
+		<div class="lower-right" :class="{active: lowerRight}">&nbsp;</div>
+		<div class="bottom" :class="{active: bottom}">&nbsp;</div>
+	</div>
+</template>
+
+<script>
+import { SetNumber } from './ScoreboardNumber.js'
+
+export default {
+	name: 'ScoreboardNumber',
+	props: {
+		number: {
+			type: Number,
+			required: true
+		},
+	},
+	watch: {
+		number: function (newNumber) {
+			SetNumber(this, newNumber);
+		}
+	},
+	data: () => ({
+		top: true,
+		upperLeft: true,
+		upperRight: true,
+		middle: true,
+		lowerLeft: true,
+		lowerRight: true,
+		bottom: true,
+	}),
+}
+</script>
+
+<style scoped>
+.ScoreboardNumber {
+	display: grid;
+	width: 100%;
+
+	grid-template-columns: 9% 78% 11%;
+	grid-template-rows: 4% 44% 4% 44% 4%;
+
+	column-gap: 0;
+	row-gap: 0;
+}
+
+div.active {
+	background-color: red;
+}
+
+div.top {
+	grid-column: 2;
+	grid-row: 1;
+}
+
+div.upper-left {
+	grid-column: 1;
+	grid-row: 2;
+}
+
+div.upper-right {
+	grid-column: 3;
+	grid-row: 2;
+}
+
+div.middle {
+	grid-column: 2;
+	grid-row: 3;
+}
+
+div.lower-left {
+	grid-column: 1;
+	grid-row: 4;
+}
+
+div.lower-right {
+	grid-column: 3;
+	grid-row: 4;
+}
+
+div.bottom {
+	grid-column: 2;
+	grid-row: 5;
+}
+</style>
