93d9926482c907b882f605cf70e042283cc31d4c
Julius Pedersen <deifyed@tutanota.com>
2022-11-06 10:55:24 +0100

    👌 Update vite config for oslo.systems domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: 'https://oslokommune.github.io/bordtennis/',
+	base: 'https://bordtennis.oslo.systems/',
   plugins: [vue()]
 })
